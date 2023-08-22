9b1dc1f4ae2b9f8cba0b217701f6a40b15d8ddaf
Ivan Klimovskiy <klimoffsky2707@yandex.ru>
2023-08-22 14:51:34 +0300

    Сделать анимацию для чисел Фибоначчи

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,55 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, { useState } from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import styles from '../fibonacci-page/fibonacci-page.module.css';
+import { Input } from '../ui/input/input';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
 
 export const FibonacciPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [circles, setCircles] = useState<number[]>([]);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const onClickHandler = () => {
+    setIsButtonClicked(true);
+    const array = [0, 1];
+    let step = 0;
+    const animation = setInterval(() => {
+      if (step >= +inputValue) {
+        clearInterval(animation);
+        setIsButtonClicked(false);
+      }
+      array.shift();
+      setCircles([...array]);
+      array.unshift(0);
+      array.push(array[step] + array[step + 1]);
+      step++;
+    }, 500);
+  };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form className={styles.form}>
+        <Input
+          type="number"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.currentTarget.value)}
+          max={19}
+          isLimitText={true}
+        />
+        <Button
+          isLoader={isButtonClicked}
+          onClick={onClickHandler}
+          disabled={+inputValue > 19 || isButtonClicked}
+          extraClass="ml-6"
+          text="Рассчитать"
+        />
+      </form>
+      <div className={styles.wrapper}>
+        {circles.map((el, index) => {
+          return <Circle extraClass="ml-8" letter={el.toString()} key={index} index={index} />;
+        })}
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,79 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, { useState } from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { RadioInput } from '../ui/radio-input/radio-input';
+import styles from './sorting-page.module.css';
+import { Button } from '../ui/button/button';
+import { Direction } from '../../types/direction';
+import { Column } from '../ui/column/column';
 
 export const SortingPage: React.FC = () => {
+  const [array, setArray] = useState<number[]>([]);
+
+  const createNewArray = () => {
+    const elementCount = Math.floor(Math.random() * (17 - 3 + 1)) + 3;
+    const tempArray: number[] = [];
+    for (let i = 0; i < elementCount; i++) {
+      const randomElement = Math.floor(Math.random() * (100 + 1));
+      tempArray.push(randomElement);
+    }
+    const newArray = [...new Set(tempArray)];
+    setArray(newArray);
+  };
+
+  const sortAscending = () => {
+    const newArray = [...array];
+    const sortedArray = [];
+    const arrayLength = newArray.length - 1;
+
+    for (let i = 0; i <= arrayLength; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j <= arrayLength; j++) {
+        if (newArray[minIndex] > newArray[j]) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        const temp = newArray[i];
+        newArray[i] = newArray[minIndex];
+        newArray[minIndex] = temp;
+      }
+
+      sortedArray.push(newArray[i]);
+    }
+
+    setArray(sortedArray);
+  };
+
+  const handleClickButton = () => {
+    createNewArray();
+  };
+  const sortingArray = () => {
+    sortAscending();
+  };
+
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={styles.wrapper}>
+        <RadioInput label={'Выбор'} />
+        <RadioInput label={'Пузырёк'} />
+        <div className={styles.buttonsWrapper}>
+          <Button
+            onClick={sortingArray}
+            extraClass={styles.button}
+            sorting={Direction.Ascending}
+            text={'По возрастанию'}
+          />
+          <Button extraClass={styles.button} sorting={Direction.Descending} text={'По убыванию'} />
+        </div>
+        <Button onClick={handleClickButton} extraClass={styles.button} text={'Новый массив'} />
+      </div>
+      <div className={styles.columnsWrapper}>
+        {array.map((el, index) => {
+          return <Column key={index} index={el} />;
+        })}
+      </div>
     </SolutionLayout>
   );
 };
